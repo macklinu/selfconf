11deeaf4eccb7423d1873e5456011bfacba84b4b
Amber Conville <crebma@gmail.com>
2014-02-25 20:25:50 -0500

    Making everything scroll to the top all the time.

--- js/main.js
+++ js/main.js
@@ -3,17 +3,24 @@ var app = angular.module('selfconf', ['ngRoute']);
 app.config(function($routeProvider) {
 
     $routeProvider.when('/', {
-        templateUrl: 'pages/home.html'
+        templateUrl: 'pages/home.html',
+        controller: 'Scrontroller'
     });
 
     $routeProvider.when('/submit', {
-        templateUrl: 'pages/submit.html'
+        templateUrl: 'pages/submit.html',
+        controller: 'Scrontroller'
     });
 
     $routeProvider.when('/coc', {
-        templateUrl: 'pages/coc.html'
+        templateUrl: 'pages/coc.html',
+        controller: 'Scrontroller'
     });
 
     $routeProvider.otherwise({ redirectTo: '/' });
 
+});
+
+app.controller('Scrontroller', function($window) {
+	$window.scrollTo(0,0);
 });
