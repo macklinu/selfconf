cc16c8ffa7508e39b893410be90274d51252f530
Amber Conville <crebma@gmail.com>
2014-04-06 19:57:11 -0400

    Adding speaker nav

--- public/js/speakers.js
+++ public/js/speakers.js
@@ -4,7 +4,7 @@ SC.speakers = (function($) {
     $.getJSON('files/speakers.json', function(speakerData) {
 
       var speakerTemplate = '<% _.forEach(speakers, function(speaker) { %>' +
-        '<li class="speaker">' +
+        '<li class="speaker" id="<%= speaker.Code %>">' +
           '<img src="<%= speaker.Headshot %>" class="img-polaroid img-rounded speaker-img pull-left">' +
           '<h4><%= speaker.Name %> – <%= speaker.Title %></h4>' +
           '<% if (speaker.Twitter) { %>' +
@@ -16,9 +16,36 @@ SC.speakers = (function($) {
           '<p><%= speaker.Bio %></p>' +
         '</li>' +
         '<% }); %>';
-
       var html = _.template(speakerTemplate, {'speakers':speakerData});
       $('.speaker-list').html(html);
+
+      var speakerNavTemplate = '<% _.forEach(speakers, function(speaker) { %>' +
+        '<li>' +
+            '<a href="#<%= speaker.Code %>"><i class="icon-chevron-right"></i><%= speaker.Name %></a>' +
+        '</li>' +
+        '<% }); %>';
+      var navData = _.sortBy(_.map(speakerData, function(speaker) {
+        return _.pick(speaker, 'Name', 'Code');
+      }), 'Name');
+      navData = _.uniq(navData,function(speaker){
+        return speaker.Name;
+      });
+      var speakerNavHtml = _.template(speakerNavTemplate, {'speakers': navData});
+      $('#names .nav').html(speakerNavHtml);
+
+      var titleNavTemplate = '<% _.forEach(speakers, function(speaker) { %>' +
+        '<li>' +
+        '<a href="#<%= speaker.Code %>"><i class="icon-chevron-right"></i><%= speaker.Title %></a>' +
+        '</li>' +
+        '<% }); %>';
+      var titleNavData = _.sortBy(_.map(speakerData, function(speaker) {
+        return _.pick(speaker, 'Title', 'Code');
+      }), 'Title');
+      titleNavData = _.uniq(titleNavData,function(speaker){
+        return speaker.Title;
+      });
+      var titleNavHtml = _.template(titleNavTemplate, {'speakers': titleNavData});
+      $('#talks .nav').html(titleNavHtml);
     });
   };
 })($);
