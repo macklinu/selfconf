cfb86ac76a1a18ee9b6addb9493f8c5de59de899
Amber Conville <crebma@gmail.com>
2014-04-06 18:18:27 -0400

    adding a config

--- main.rb
+++ main.rb
@@ -2,17 +2,19 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
-get '/' do
-  @title = 'Self.conference'
-  erb :main
-end
+class SelfConf < Sinatra::Application
+  get '/' do
+    @title = 'Self.conference'
+    erb :main
+  end
 
-get '/coc' do
-  @title = 'Code of Conduct | Self.conference'
-  erb :coc
-end
+  get '/coc' do
+    @title = 'Code of Conduct | Self.conference'
+    erb :coc
+  end
 
-get '/speakers' do
-  @title = 'Speakers | Self.conference'
-  erb :speakers
+  get '/speakers' do
+    @title = 'Speakers | Self.conference'
+    erb :speakers
+  end
 end
