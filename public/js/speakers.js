728a9a30a6cec971d67407f9acf60ee4368af388
Amber Conville <crebma@gmail.com>
2014-04-15 08:57:47 -0400

    fixing session linking

--- public/js/speakers.js
+++ public/js/speakers.js
@@ -4,16 +4,18 @@ SC.speakers = (function($) {
     $.getJSON('files/speakers.json', function(speakerData) {
 
       var speakerTemplate = '<% _.forEach(speakers, function(speaker) { %>' +
-        '<li class="speaker" id="<%= speaker.Code %>">' +
-          '<img src="<%= speaker.Headshot %>" class="img-polaroid img-rounded speaker-img pull-left">' +
-          '<h4><%= speaker.Name %> – <%= speaker.Title %></h4>' +
-          '<% if (speaker.Twitter) { %>' +
-          '<h5 class="italic"><a href="http://twitter.com/<%= speaker.Twitter %>"><%= speaker.Twitter %></a></h5>' +
-          '<% } %>' +
-          '<p><%= speaker.Description %></p>' +
+        '<li class="speaker" id="<%= speaker.name_anchor %>">' +
+          '<div id="<%= speaker.talk_anchor %>">' +
+            '<img src="<%= speaker.headshot %>" class="img-polaroid img-rounded speaker-img pull-left">' +
+            '<h4><%= speaker.name %> – <%= speaker.title %></h4>' +
+            '<% if (speaker.twitter) { %>' +
+            '<h5 class="italic"><a href="http://twitter.com/<%= speaker.Twitter %>"><%= speaker.twitter %></a></h5>' +
+            '<% } %>' +
+            '<p><%= speaker.description %></p>' +
 
-          '<h5 class="italic">About <%= speaker.Name %>:</h5>' +
-          '<p><%= speaker.Bio %></p>' +
+            '<h5 class="italic">About <%= speaker.name %>:</h5>' +
+            '<p><%= speaker.bio %></p>' +
+          '</div>' +
         '</li>' +
         '<% }); %>';
       var html = _.template(speakerTemplate, {'speakers':speakerData});
@@ -21,29 +23,23 @@ SC.speakers = (function($) {
 
       var speakerNavTemplate = '<% _.forEach(speakers, function(speaker) { %>' +
         '<li>' +
-            '<a href="#<%= speaker.Code %>"><i class="icon-chevron-right"></i><%= speaker.Name %></a>' +
+            '<a href="#<%= speaker.name_anchor %>"><i class="icon-chevron-right"></i><%= speaker.name %></a>' +
         '</li>' +
         '<% }); %>';
       var navData = _.sortBy(_.map(speakerData, function(speaker) {
-        return _.pick(speaker, 'Name', 'Code');
-      }), 'Name');
-      navData = _.uniq(navData,function(speaker){
-        return speaker.Name;
-      });
+        return _.pick(speaker, 'name', 'name_anchor');
+      }), 'name');
       var speakerNavHtml = _.template(speakerNavTemplate, {'speakers': navData});
       $('#names .nav').html(speakerNavHtml);
 
       var titleNavTemplate = '<% _.forEach(speakers, function(speaker) { %>' +
         '<li>' +
-        '<a href="#<%= speaker.Code %>"><i class="icon-chevron-right"></i><%= speaker.Title %></a>' +
+        '<a href="#<%= speaker.talk_anchor %>"><i class="icon-chevron-right"></i><%= speaker.title %></a>' +
         '</li>' +
         '<% }); %>';
       var titleNavData = _.sortBy(_.map(speakerData, function(speaker) {
-        return _.pick(speaker, 'Title', 'Code');
-      }), 'Title');
-      titleNavData = _.uniq(titleNavData,function(speaker){
-        return speaker.Title;
-      });
+        return _.pick(speaker, 'title', 'talk_anchor');
+      }), 'title');
       var titleNavHtml = _.template(titleNavTemplate, {'speakers': titleNavData});
       $('#talks .nav').html(titleNavHtml);
     });
