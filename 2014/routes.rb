9d9b0e69b2287dbdd4ec4487c6dee5941cf7de63
Amber Conville <crebma@gmail.com>
2014-09-22 19:37:55 -0400

    Move 2014 routes somewhere else

---
+++ 2014/routes.rb
@@ -0,0 +1,30 @@
+get '/2014', :layout => false do
+  @title = 'Self.conference'
+
+  erb :'2014/main', :layout => :'2014/layout'
+end
+
+get '/2014/coc', :layout => false do
+  @title = 'Code of Conduct | Self.conference'
+  erb :'2014/coc', :layout => :'2014/layout'
+end
+
+get '/2014/speakers', :layout => false do
+  @title = 'Speakers | Self.conference'
+  @speakers = JSON.parse(File.read('public/2014/files/speakers.json'))
+  @speaker_nav = @speakers.map { | speaker | { :name => speaker['name'], :anchor => speaker['name_anchor'] } }.sort_by { | speaker | speaker[:name] }
+  @talk_nav = @speakers.map { | speaker | { :title => speaker['title'], :anchor => speaker['talk_anchor'] } }.sort_by { | speaker | speaker[:title] }
+  erb :'2014/speakers', :layout => :'2014/layout'
+end
+
+get '/2014/schedule', :layout => false do
+  @title = 'Schedule | Self.conference'
+  talks = JSON.parse(File.read('public/2014/files/schedule.json'))
+  schedule = talks.sort_by { | talk | [DateTime.strptime(talk['beginning'], '%m/%d/%Y %H:%M:%S'), talk['room']] }.group_by { | thing | thing['beginning'] }
+
+  @rooms = %w(Ballroom 140A 140B 140C 140D 140E 140F 140G)
+  @friday = schedule.select{|key| key.include? '05/30/2014' }
+  @saturday = schedule.select{|key| key.include? '05/31/2014' }
+
+  erb :'2014/schedule', :layout => :'2014/layout'
+end
